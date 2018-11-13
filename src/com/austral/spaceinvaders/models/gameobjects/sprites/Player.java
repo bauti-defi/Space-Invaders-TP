be202be39f547d655a37e507ea4ca0e24da7465b
Bautista Baiocchi-Lora <Bautista-Baiocchi-lora@users.noreply.github.com>
2018-11-12 22:35:46 -0300

    -Shot id generation is now multithread friendly

--- src/com/austral/spaceinvaders/models/gameobjects/sprites/Player.java
+++ src/com/austral/spaceinvaders/models/gameobjects/sprites/Player.java
@@ -4,9 +4,12 @@ import com.austral.spaceinvaders.models.gameobjects.Controllable;
 import com.austral.spaceinvaders.physics.Direction;
 import com.austral.spaceinvaders.physics.Velocity;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Player extends Character implements Controllable {
 
 	private static final String PLAYER_IMAGE = "src/images/player.png";
+	private final AtomicInteger shotIdGenerator = new AtomicInteger(0);
 
 	public Player(int x, int y, int health) {
 		super(x, y, health, PLAYER_IMAGE);
@@ -14,11 +17,11 @@ public class Player extends Character implements Controllable {
 
 	@Override
 	public Shot fire() {
-		return new Shot(getX() + (getWidth() / 2), getY());
+		return new Shot(getX() + (getWidth() / 2), getY(), shotIdGenerator.getAndIncrement());
 	}
 
 	public Shot secondaryFire() {
-		return new Shot(getX() + (getWidth() / 4), getY());
+		return new Shot(getX() + (getWidth() / 4), getY(), shotIdGenerator.getAndIncrement());
 	}
 
 	@Override
--- src/com/austral/spaceinvaders/models/gameobjects/sprites/Shot.java
+++ src/com/austral/spaceinvaders/models/gameobjects/sprites/Shot.java
@@ -3,16 +3,14 @@ package com.austral.spaceinvaders.models.gameobjects.sprites;
 import com.austral.spaceinvaders.physics.Direction;
 import com.austral.spaceinvaders.physics.Velocity;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Shot extends Projectile {
 
 	private static final String SHOT_IMAGE = "src/images/shot.png";
-	private static final AtomicInteger idGenerator = new AtomicInteger(0);
-	private final int id = idGenerator.getAndIncrement();
+	private final int id;
 
-	public Shot(final int x, final int y) {
+	public Shot(final int x, final int y, final int id) {
 		super(x, y, new Velocity(), new Velocity(5, Direction.NORTH), 50, SHOT_IMAGE);
+		this.id = id;
 	}
 
 	public int getId() {
